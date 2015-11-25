69c691078beebc061be80bccf07f5feb6de1cf7e
Myselfminer <david@divaproject.com>
2015-11-25 23:09:03 +0100

    Create translate.py

---
+++ translate.py
@@ -0,0 +1,7 @@
+def do(self):
+    self.components.Open.label="Open"
+    self.components.Delete.label="Delete"
+    self.components.Duplicate.label="Duplicate"
+    self.components.Run.label="Run"
+    self.components.Files.text="Files"
+    self.components.Call.label="Call"
